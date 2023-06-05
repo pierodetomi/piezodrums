a8a5ef37e9f2eff7562877fb12e46b24021450f7
Piero De Tomi <piero.detomi@ikon.it>
2023-06-05 14:00:52 +0200

    Refactoring & code comments

--- PiezoDrums/Utilities/WaveformAnalyzer.cs
+++ PiezoDrums/Utilities/WaveformAnalyzer.cs
@@ -8,50 +8,67 @@ namespace PiezoDrums.Utilities
 
         private bool _isInsideWave = false;
 
-        private Action<AudioSamplePeakValue> _onWavePeakDetected;
+        private Action<AudioSamplePeakValue> _onWaveMaxPeakDetected;
 
-        public WaveformAnalyzer(Action<AudioSamplePeakValue> onWavePeakDetected)
+        public WaveformAnalyzer(Action<AudioSamplePeakValue> onWaveMaxPeakDetected)
         {
-            _onWavePeakDetected = onWavePeakDetected;
+            _onWaveMaxPeakDetected = onWaveMaxPeakDetected;
         }
 
-        public void AddSamplePeakValue(AudioSamplePeakValue waveValue)
+        public void AddSamplePeakValue(AudioSamplePeakValue samplePeak)
         {
             if (!_isInsideWave)
             {
-                if (waveValue.IsAudible)
+                if (samplePeak.IsAudible)
                 {
-                    _samplePeaksQueue.Add(waveValue);
+                    _samplePeaksQueue.Add(samplePeak);
                     _isInsideWave = true;
                 }
             }
             else
             {
-                if (waveValue.IsAudible)
+                if (samplePeak.IsAudible)
                 {
-                    _samplePeaksQueue.Add(waveValue);
+                    _samplePeaksQueue.Add(samplePeak);
                 }
                 else
                 {
-                    // Calculate wave peak
-                    var max = _samplePeaksQueue[0];
+                    /*
+                     * Calculate wave peak
+                     * 
+                     * This is given by the greatest peak value between collected samples.
+                     * 
+                     */
+                    var maxPeak = GetWaveMaxPeak();
 
-                    for (var i = 1; i < _samplePeaksQueue.Count; i++)
-                    {
-                        var sampleValue = _samplePeaksQueue[i];
-                        
-                        if (sampleValue.NormalizedValue > max.NormalizedValue)
-                            max = sampleValue;
-                    }
-
-                    // Emit wave peak value
-                    _onWavePeakDetected(max);
+                    /* Emit wave MAX peak value.
+                     * 
+                     * For performance purposes, this callback will be executed async, to avoid blocking/slowing 
+                     * the processing of future incoming samples.
+                     * 
+                     */
+                    Task.Run(() => _onWaveMaxPeakDetected(maxPeak));
 
                     // Clear state
                     _samplePeaksQueue.Clear();
                     _isInsideWave = false;
                 }
             }
+        }
+
+        private AudioSamplePeakValue GetWaveMaxPeak()
+        {
+            var max = _samplePeaksQueue[0];
+
+            for (var i = 1; i < _samplePeaksQueue.Count; i++)
+            {
+                var sampleValue = _samplePeaksQueue[i];
+
+                if (sampleValue.NormalizedValue > max.NormalizedValue)
+                    max = sampleValue;
+            }
+
+            return max;
         }
     }
 }
